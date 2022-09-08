497a24a0d88ae209b34b2fe7e06a2232e40629c7
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2022-09-08 18:53:24 +0200

    Update app.py
    uploaded latest version of app file

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask
+from dotenv import load_dotenv
+import os
 from flask import render_template, request, url_for, redirect
 from forms import AlbumSearchForm
 
-CLIENT_ID = '1e68bf09982545789b04249ccb8057fa'
-CLIENT_SECRET = '19bb799a646f4c5aa89c0e582e8a5069'
-SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5000/callback'
-SPOTIFY_USER_ID = '31rxmn3ikrqfqxwjuoplp2u4dnk4'
+load_dotenv()  # load the environment variables
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
+SPOTIFY_USER_ID = os.getenv('SPOTIFY_USER_ID')
 
 scope = "user-library-read user-top-read playlist-modify-public user-follow-read user-library-read " \
         "playlist-read-private playlist-modify-private "
@@ -16,9 +20,3 @@ app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
 
 import routes
-
-"""
-albums = sp.artist_albums('7bXgB6jMjp9ATFy66eO08Z', album_type=None, country='US', limit=10)
-print(albums)
-return "Moving Forward!"
-    """
