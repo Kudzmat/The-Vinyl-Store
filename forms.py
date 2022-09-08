bb8fc0f85adea5d5a430dcef435cf4bb7d7e53ca
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2022-09-08 18:56:39 +0200

    Update forms.py
    uploaded latest version of forms

--- forms.py
+++ forms.py
@@ -3,12 +3,13 @@ from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 
+# album search form
 class AlbumSearchForm(FlaskForm):
     album = StringField("Enter Album Name", validators=[DataRequired()])
     submit = SubmitField("Search For This Album")
 
 
+# artist search form
 class ArtistSearchForm(FlaskForm):
     name = StringField("Enter Artist Name", validators=[DataRequired()])
     submit = SubmitField("Search For This Artist")
-
