e3c847dedf0c9b9f024026e5814b2f217ad6e9df
Bam_boogie <65554208+Kudzmat@users.noreply.github.com>
2023-02-15 15:45:31 +0200

    Update forms.py
    Added more string fields for the playlist submission

--- forms.py
+++ forms.py
@@ -17,5 +17,9 @@ class ArtistSearchForm(FlaskForm):
 
 # playlist request form
 class PlaylistForm(FlaskForm):
-    artists = StringField("Enter Artists", validators=[DataRequired()])
+    artist1 = StringField("First Artist", validators=[DataRequired()])
+    artist2 = StringField("Second Artist", validators=[DataRequired()])
+    artist3 = StringField("Third Artist", validators=[DataRequired()])
+    artist4 = StringField("Fourth Artist", validators=[DataRequired()])
+    artist5 = StringField("Fifth Artist", validators=[DataRequired()])
     submit = SubmitField("Check My Vibe!")
